0cba9cb1d8161296aca2cd1c76faa6eccb96f47a
Alex <113828515+alekssm@users.noreply.github.com>
2025-05-20 22:46:19 +0300

    Continue the Pandas basic introduction

--- day-25-CSV-data/pandas_test.py
+++ day-25-CSV-data/pandas_test.py
@@ -18,8 +18,34 @@ ages = pd.Series([13, 19, 21], name="age")
 
 #Read csv file and creates a DF from the data
 titanic = pd.read_csv("titanic_data.csv")
-print(titanic)
 
 #Creates and writes an Excel file from the data
 #titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
 
+#Selects a specific column from the DF / returns a Series
+age = titanic.Age
+
+survived = titanic["Survived"]
+
+#Selects more than one column from DF / returns a DF
+age_and_sex = titanic[["Age", "Sex"]]
+
+#Selects and returns only thr rows where the condition is True / In the example is the value in column "Age" > 35
+above_35 = titanic[titanic.Age > 35]
+
+searched_classes = [2, 3]
+#Selects and returns only the rows where the condition is True / isin([]) - is the value of the column in the provided list
+class_2_3 = titanic[titanic.Pclass.isin(searched_classes)]
+
+#Selects and returns only the rows where the condition is not Null / notna()
+known_class = titanic[titanic.Pclass.notna()]
+
+#Selects and returns only the column of the passengers who are above 35
+above_35_names = titanic.loc[titanic.Age > 35, "Name"]
+
+#Select and returns only the columns chosen based on index in the FD
+ten_to_25 = titanic.iloc[9:24, 4:6]
+print(ten_to_25)
+
+#It will assign the value to the chosen rows and columns cells
+#titanic.iloc[0:3, 1:5] = "koza"
