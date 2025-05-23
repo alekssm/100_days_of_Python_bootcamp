ab0f43800e122fb30ef0569b9fcba0a1147ff313
Alex <113828515+alekssm@users.noreply.github.com>
2025-05-23 23:44:26 +0300

    Day 27 start

---
+++ day-27-args-kwargs-tkinter/main.py
@@ -0,0 +1,18 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="Label", font=("Arial", 24))
+my_label.pack()
+
+
+
+
+
+
+
+
+window.mainloop()
