e55497717e590e4fe239c77167f240611c1cdecf
Alex <113828515+alekssm@users.noreply.github.com>
2025-05-31 22:58:10 +0300

    Day 26 Nato word update with Exception handling

--- day-26-list-comprehension/nato_alphabet.py
+++ day-26-list-comprehension/nato_alphabet.py
@@ -4,9 +4,17 @@ nato_alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet = {row.letter:row.code for index, row in nato_alphabet_df.iterrows()}
 
-user_word = input("Write a word: ").upper()
+def test_input():
+    try:
+        user_word = input("Write a word: ").upper()
+        hidden_word = [nato_alphabet[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please!")
+        test_input()
+    else:
+        print(hidden_word)
 
-hidden_word = [nato_alphabet[letter] for letter in user_word]
 
-print(hidden_word)
+test_input()
+
 
